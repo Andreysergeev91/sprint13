88aec36c67a1848a7951271c84c9bf9ed559d632
Andrey Sergeev <andreyser07@gmail.com>
2020-05-16 18:23:22 +0700

    Add router "users"
    Router users.js was added

--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,11 @@
 const usersRouter = require('express').Router();
 
-const fsPromises = require('fs').promises;
+const {getUsers, getUsersById, createUser} = require('../controllers/users');
 
-const path = require('path');
+usersRouter.get('/users', getUsers);
 
-const usersPath = path.join('data', 'users.json');
+usersRouter.get('/users/:userId', getUsersById);
 
-usersRouter.get('/users', (req, res) => {
-  fsPromises.readFile(usersPath, { encoding: 'utf8' })
-    .then((data) => {
-      res.send(JSON.parse(data));
-    })
-    .catch((err) => {
-      res.status(500);
-      res.send(err.message);
-    });
-});
-
-usersRouter.get('/users/:id', (req, res) => {
-  const { id } = req.params;
-
-  fsPromises.readFile(usersPath, { encoding: 'utf8' })
-    .then((data) => {
-      if (!(JSON.parse(data).some((item) => item._id === id))) {
-        res.send({ message: 'Нет пользователя с таким id' });
-        return;
-      }
-      res.send(JSON.parse(data).find((item) => item._id === id));
-    })
-    .catch((err) => {
-      res.status(500);
-      res.send(err.message);
-    });
-});
+usersRouter.post('/users', createUser);
 
 module.exports = usersRouter;
