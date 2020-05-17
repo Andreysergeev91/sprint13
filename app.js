6b9be003cfbca2c3883d6e56c6b939f4eac316e2
Andrey Sergeev <andreyser07@gmail.com>
2020-05-17 16:00:51 +0700

    Add additional middleware
    Middleware for user id  was added.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const {
-  PORT = 3000,
+  PORT = 3000
 } = process.env;
 
 
@@ -18,16 +18,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useFindAndModify: false,
+  useFindAndModify: false
 });
 
 
-app.use('/users', require('./routes/users'));
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5ebfdacaffe2ad415c961b1d'
+  };
+
+  next();
+});
+
+app.use('/', require('./routes/users'));
+app.use('/', require('./routes/cards'));
 
 
 app.get('*', (req, res) => {
   res.status(404).send({
-    message: 'Запрашиваемый ресурс не найден',
+    message: 'Запрашиваемый ресурс не найден'
   });
 });
 
