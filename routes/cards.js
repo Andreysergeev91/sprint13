20d4bf7208786d73e0cb263605b82717c041d5ff
Andrey Sergeev <andreyser07@gmail.com>
2020-05-17 16:11:20 +0700

    Change cardsRouter
    CardsRouter was changed due to controllers were added.

--- routes/cards.js
+++ routes/cards.js
@@ -1,21 +1,10 @@
 const cardsRouter = require('express').Router();
 
-const fsPromises = require('fs').promises;
-
-const path = require('path');
-
-const cardsPath = path.join('data', 'cards.json');
+const { getCards, createCard, deleteCard } = require('../controllers/cards');
 
 
-cardsRouter.get('/cards', (req, res) => {
-  fsPromises.readFile(cardsPath, { encoding: 'utf8' })
-    .then((data) => {
-      res.send(JSON.parse(data));
-    })
-    .catch((err) => {
-      res.status(500);
-      res.send(err.message);
-    });
-});
+cardsRouter.get('/cards', getCards);
+cardsRouter.post('/cards', createCard);
+cardsRouter.delete('/cards/:cardId', deleteCard);
 
 module.exports = cardsRouter;
